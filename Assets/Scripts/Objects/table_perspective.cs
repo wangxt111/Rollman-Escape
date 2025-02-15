a8fc0f4201e43936cc62cbf404ef37bbb6d900b3
wxt <wang-xt23@mails.tsinghua.edu.cn>
2025-02-15 23:58:47 +0800

    fix: table

--- Assets/Scripts/Objects/table_perspective.cs
+++ Assets/Scripts/Objects/table_perspective.cs
@@ -6,36 +6,12 @@ using UnityEngine;
 public class Table_perspective_close : MonoBehaviour
 {
     Vector3 CameraPositionAfterClick = new Vector3(0, 32, 0);
-    public Sprite safebox_open;
-    private Renderer[] childRenderers;
     void Start()
     {
-        childRenderers = GetComponentsInChildren<Renderer>(true);
-        foreach (Renderer renderer in childRenderers)
-        {
-            if (renderer.gameObject != this.gameObject)
-            {
-                renderer.enabled = false;
-            }
-        }
         return;
     }
     void Update()
     {
-        if( info.safe_box == 0 || info.safe_box == 1 ) CameraPositionAfterClick = new Vector3(0, 32, 0);
-        if( info.safe_box == 2 )
-        {
-            Collider collider = GetComponent<Collider>();
-            collider.enabled = false;
-            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-            spriteRenderer.sprite = safebox_open;
-            childRenderers = GetComponentsInChildren<Renderer>(true);
-            foreach (Renderer renderer in childRenderers)
-            {
-                renderer.enabled = true;
-            }
-            return;
-        }
         // 检测鼠标左键是否被按下
         if (Input.GetMouseButtonDown(0))
         {
@@ -53,7 +29,7 @@ public class Table_perspective_close : MonoBehaviour
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
             // 如果射线击中了当前物体
-            if (hit.collider != null && hit.collider.gameObject == this.gameObject && info.safe_box != 2)
+            if (hit.collider != null && hit.collider.gameObject == this.gameObject )
             {
                 Utils.MoveCamera(CameraPositionAfterClick,true);
             }
